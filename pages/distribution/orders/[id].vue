b789add5ab0978a3c09e3a135f08fc163afc55bb
Happy-Lama <kldenis2001@gmail.com>
2024-09-06 21:28:35 +0300

    Added orders page on distribution and fixed bugs

--- pages/distribution/orders/[id].vue
+++ pages/distribution/orders/[id].vue
@@ -7,36 +7,11 @@
                 </v-card-title>
                 <v-container fluid>
                     <v-row>
-                        <v-col>
-                            <v-card class="pa-2 ma-2 bg-success" variant="flat" style="height: 12rem;">
-                                <v-card-title class="text-h5 success">
-                                    Full
-                                </v-card-title>
-                                <v-card-text class="text-h3">
-                                    {{ inventory_full }}
-                                </v-card-text>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card class="pa-2 ma-2 bg-primary" variant="flat" style="height: 12rem;">
-                                <v-card-title class="text-h5 info">
-                                    Empty
-                                </v-card-title>
-                                <v-card-text class="text-h3">
-                                    {{ inventory_empty }}
-                                </v-card-text>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card class="pa-2 ma-2 bg-warning" variant="flat" style="height: 12rem;">
-                                <v-card-title class="text-h5 error">
-                                    Defective
-                                </v-card-title>
-                                <v-card-text class="text-h3">
-                                    {{ inventory_defect }}
-                                </v-card-text>
-                            </v-card>
-                        </v-col>
+                        <v-data-table-virtual
+                            :items="ordersData"
+                            height="100%"
+                            item-value="name"
+                        ></v-data-table-virtual>
                     </v-row>
                 </v-container>
             </v-card>
@@ -55,51 +30,28 @@ import { doc, collection, onSnapshot} from 'firebase/firestore';
 const nuxtApp = useNuxtApp();
 const route = useRoute();
 
-const inventory_full = ref(0);
-const inventory_defect = ref(0);
-const inventory_empty = ref(0);
-const shipment_out = ref([]);
-const shipment_in = ref([]);
+const ordersData = ref([]);
 
 const unsub = onSnapshot(doc(nuxtApp.$firestore, 'distributionCentres', route.params.id), (doc) => {
-    console.log('Current data', doc.data())
-    inventory_full.value = 0;
-    inventory_empty.value = 0;
-    inventory_defect.value = 0;
-    if(doc.data().stockIn){
-        doc.data().stockIn.forEach((stock) => {
-            if(stock.cylinders){
-                if(stock.cylinders[0].status === 'Full'){
-                    inventory_full.value += 1;
-                } else if (stock.cylinders[0].status === 'Empty'){
-                    inventory_empty.value += 1;
-                } else {
-                    inventory_defect.value += 1;
+    let orders = []
+    if(doc.data().orders){
+        doc.data().orders.forEach((order) => {
+            if(order.orderStatus){
+                if(order.orderStatus === 'Pending'){
+                    let order_modified = {}
+                    Object.keys(order).forEach((key) => {
+                        if(key !== 'items'){
+                            order_modified[key] = order[key]
+                        }
+                    })
+                    orders.push(order_modified)
                 }
             }
-            
         })
     }
     
-    let stockOut = [];
-    if(doc.data().stockOut){
-        doc.data().stockOut.forEach((stock) => {
-            if(stock){
-                let stockDetails = {
-                    shipId: stock.shipId,
-                    // from: doc.id,
-                    // to: stock.to.location,
-                    cylinders: stock.cylinders.length, 
-                    createdOn: stock.date
-                }
-                if(!stockOut.includes(stockDetails)){
-                    stockOut.push(stockDetails);
-                }
-            }        
-        })
-    }
     
-    shipment_out.value = stockOut;
+    ordersData.value = orders;
 })
 
 </script>
